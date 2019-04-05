6e2b7c6a2c02ff815b27860984bdc564dcbbf0ca
esuau <evan.suau@etu.u-pec.fr>
2019-04-05 17:59:34 +0200

    feat(parallel): Count number of words in file with MPJ

--- src/main/java/com/esipe/fr/Utils.java
+++ src/main/java/com/esipe/fr/Utils.java
@@ -1,19 +1,20 @@
 package com.esipe.fr;
 
 import mpi.MPI;
+
 import java.io.*;
 
-public class Utils {
+class Utils {
 
-    public static void readFile(String[] args) throws Exception {
+    static void readFile(String[] args) throws Exception {
 
-        File file = new File(args[0]);
+        File file = new File(args[4]);
 
         if (!file.exists()) {
             throw new Exception("File doesn't exists");
         }
 
-        String word = args[1];
+        String word = args[5];
 
         try {
             FileReader fileReader = new FileReader(file);
@@ -27,51 +28,46 @@ public class Utils {
             int destination = 0;
             int source;
 
-            if(rank == 0) {
-                int totalOccurs = 0;
-                String line = bufferedReader.readLine();
+            String line = bufferedReader.readLine();
+            System.out.println(rank);
 
-                while (line != null) {
-                    line = bufferedReader.readLine();
+            if (rank == 0) {
+                int totalOccurs = 0;
+
+                while ((line = bufferedReader.readLine()) != null) {
 
                     destination = destination == size ? 1 : destination + 1;
 
                     int lineOccurs = 0;
 
-                    MPI.COMM_WORLD.Send(line, 0, line.length(), MPI.OBJECT,
-                            destination, 0);
-                    MPI.COMM_WORLD.Recv(lineOccurs, 0, 0, MPI.OBJECT,
-                            destination, 0);
+                    MPI.COMM_WORLD.Send(line, 0, line.length(), MPI.OBJECT, destination, 0);
+                    MPI.COMM_WORLD.Recv(lineOccurs, 0, 0, MPI.OBJECT, destination, 0);
 
                     totalOccurs += lineOccurs;
                 }
+
+                System.out.println(totalOccurs);
             } else {
-                while(true) {
+                while (true) {
                     int lineOccurs = 0;
-                    String line = "";
+                    String lineToReceive = "";
 
                     // To return to original Processor equivalent to destination for Sending back lineOccurs
                     source = 0;
 
-                    MPI.COMM_WORLD.Recv(line, 0, line.length() , MPI.OBJECT,
+                    MPI.COMM_WORLD.Recv(lineToReceive, 0, lineToReceive.length(), MPI.OBJECT,
                             source, 0);
 
-                    for (String wordOfLine : line.split(" ")) {
+                    for (String wordOfLine : lineToReceive.split(" ")) {
                         if (wordOfLine.equals(word)) {
                             lineOccurs++;
                         }
                     }
 
-                    MPI.COMM_WORLD.Send(lineOccurs, 0, 0, MPI.OBJECT,
-                            source, 0);
+                    MPI.COMM_WORLD.Send(lineOccurs, 0, 0, MPI.OBJECT, source, 0);
                 }
-
             }
-        } catch (
-                FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (
-                IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } finally {
             MPI.Finalize();
